cc7d4185d552a9fb7d403ac17942fb4124572c05
Patrick Spek <p.spek@tyil.nl>
2014-05-07 18:49:40 +0200

    Created a userlist and a possibility for admins to see a user's account, though it cannot be saved yet

---
+++ app/composers/UserEditComposer.php
@@ -0,0 +1,16 @@
+<?php
+
+class UserEditComposer {
+	public function compose($view) {
+		// Get all the themes from the directory
+		$themeDir = array_diff(scandir(base_path().'/public/css/themes'), ['.', '..']);
+
+		// Make it into an array for our use
+		foreach($themeDir as $theme) {
+			$t = substr($theme, 0, -4);
+			$themes[$t] = $t;
+		}
+
+		$view->with('themes', $themes);
+	}
+}
