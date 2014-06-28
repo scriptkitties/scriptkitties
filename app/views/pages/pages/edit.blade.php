a0f12a29693a0e5dd934973fdd998e13eebe552f
Patrick Spek <p.spek@tyil.nl>
2014-06-28 14:00:13 +0200

    Fixed the form for the page-edit page

--- app/views/pages/pages/edit.blade.php
+++ app/views/pages/pages/edit.blade.php
@@ -1,19 +1,21 @@
 @extends('layout')
 
 @section('main')
-{{ Form::open() }}
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="form-group">
-					{{ Form::label('content', 'Page contents') }}
-					{{ Form::textarea('content', isset($page) ? $page->content : '', ['class' => 'form-control']) }}
-				</div>
-				<div class="form-group">
-					{{ Form::submit('Update page contents', ['class' => 'btn btn-default']) }}
+<section class="content">
+	{{ Form::open() }}
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="form-group">
+						{{ Form::label('content', 'Page contents') }}
+						{{ Form::textarea('content', isset($page) ? $page->content : '', ['class' => 'form-control']) }}
+					</div>
+					<div class="form-group">
+						{{ Form::submit('Update page contents', ['class' => 'btn btn-default']) }}
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</div>
+	</form>
+</section>
 @stop
