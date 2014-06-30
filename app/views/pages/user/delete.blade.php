7fe60c82f7f00ed59f9534bebf04b24586d5f628
Patrick Spek <p.spek@tyil.nl>
2014-06-30 18:36:05 +0200

    updated the delete user and update permission pages

--- app/views/pages/user/delete.blade.php
+++ app/views/pages/user/delete.blade.php
@@ -1,28 +1,35 @@
 @extends('layout')
 
 @section('main')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			{{ Form::open() }}
-				<div class="form-group">
-					<p>
-						This deletes the user, his permissions and preferences. This action <strong>CAN NOT</strong> be
-						undone. Be very careful with this feature. It's use will be logged, and improper use will lead
-						to immediate removal of your account.
-					</p>
-					<div class="checkbox">
-						<label>
-							{{ Form::checkbox('delete', $user->id) }}
-							I want to delete <strong>{{$user->nickname}}</strong>
-						</label>
+<section class="content">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Deleting {{$user->nickname}}</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				{{ Form::open() }}
+					<div class="form-group">
+						<p>
+							This deletes the user, his permissions and preferences. This action <strong>CAN NOT</strong> be
+							undone. Be very careful with this feature. It's use will be logged, and improper use will lead
+							to immediate removal of your account.
+						</p>
+						<div class="checkbox">
+							<label>
+								{{ Form::checkbox('delete', $user->id) }}
+								I want to delete <strong>{{$user->nickname}}</strong>
+							</label>
+						</div>
+						<div class="form-group pull-right">
+							{{ Form::submit('Yes, I am sure', ['class' => 'btn btn-danger']) }}
+						</div>
 					</div>
-					<div class="form-group pull-right">
-						{{ Form::submit('Yes, I am sure', ['class' => 'btn btn-danger']) }}
-					</div>
-				</div>
-			</form>
+				</form>
+			</div>
 		</div>
 	</div>
-</div>
+</section>
 @stop
--- app/views/pages/user/permissions.blade.php
+++ app/views/pages/user/permissions.blade.php
@@ -1,55 +1,57 @@
 @extends('layout')
 
 @section('main')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<p>Editing permissions of <strong>{{ $user->nickname }}</strong>.</p>
+<section class="content">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Editing permissions of <strong>{{ $user->nickname }}</strong></h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				{{ Form::open() }}
+					<div class="form-group">
+						<table class="table">
+							<thead>
+								<tr>
+									<th>Permissionlist</th>
+									<th>Read</th>
+									<th>Write</th>
+									<th>Admin</th>
+								</tr>
+							</thead>
+							<tbody>
+								@foreach($permlist as $perm => $desc)
+								<tr>
+									{{ Form::hidden('perms[]', $perm) }}
+									<td>{{ $desc }}</td>
+									<td>
+										<div class="checkbox">
+											{{ Form::checkbox($perm.'_read', '1', $user->hasPermission($perm, 'r')) }}
+										</div>
+									</td>
+									<td>
+										<div class="checkbox">
+											{{ Form::checkbox($perm.'_write', '1', $user->hasPermission($perm, 'w')) }}
+										</div>
+									</td>
+									<td>
+										<div class="checkbox">
+											{{ Form::checkbox($perm.'_admin', '1', $user->hasPermission($perm, 'a')) }}
+										</div>
+									</td>
+								</tr>
+								@endforeach
+							</tbody>
+						</table>
+					</div>
+					<div class="form-group">
+						{{ Form::submit('Save permissions', ['class' => 'btn btn-default']) }}
+					</div>
+				{{ Form::close() }}
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-12">
-			{{ Form::open() }}
-				<div class="form-group">
-					<table class="table">
-						<thead>
-							<tr>
-								<th>Permissionlist</th>
-								<th>Read</th>
-								<th>Write</th>
-								<th>Admin</th>
-							</tr>
-						</thead>
-						<tbody>
-							@foreach($permlist as $perm => $desc)
-							<tr>
-								{{ Form::hidden('perms[]', $perm) }}
-								<td>{{ $desc }}</td>
-								<td>
-									<div class="checkbox">
-										{{ Form::checkbox($perm.'_read', '1', $user->hasPermission($perm, 'r')) }}
-									</div>
-								</td>
-								<td>
-									<div class="checkbox">
-										{{ Form::checkbox($perm.'_write', '1', $user->hasPermission($perm, 'w')) }}
-									</div>
-								</td>
-								<td>
-									<div class="checkbox">
-										{{ Form::checkbox($perm.'_admin', '1', $user->hasPermission($perm, 'a')) }}
-									</div>
-								</td>
-							</tr>
-							@endforeach
-						</tbody>
-					</table>
-				</div>
-				<div class="form-group">
-					{{ Form::submit('Save permissions', ['class' => 'btn btn-default']) }}
-				</div>
-			{{ Form::close() }}
-		</div>
-	</div>
-</div>
+</section>
 @stop
